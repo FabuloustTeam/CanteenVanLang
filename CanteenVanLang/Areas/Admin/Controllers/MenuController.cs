74ea53baea4f2317ff6a69c719a092d0538bff08
187PM13983 - Phan Nguyen Thi Yen Nhi - K24T-PM1 <nhi.187pm13983@vanlanguni.vn>
2021-01-08 15:38:12 +0700

    add view create

---
+++ CanteenVanLang/Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CanteenVanLang.Models;
+
+namespace CanteenVanLang.Areas.Admin.Controllers
+{
+    public class MenuController : Controller
+    {
+        QUANLYCANTEENEntities model = new QUANLYCANTEENEntities();
+        // GET: Admin/Menu
+        public ActionResult Index()
+        {
+            var menu = model.MENUs.OrderByDescending(men => men.DATE).ToList();
+            return View(menu);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Foods = model.FOODs.OrderBy(food => food.FOOD_NAME).ToList();
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Create(MENU newMenu)
+        //{
+
+        //}
+    }
+}
