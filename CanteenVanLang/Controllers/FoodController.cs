3b15843acee8a7519e9b2a2506c95236822b245e
LanPhuong <phuong.187pm13997@vanlanguni.vn>
2021-01-08 17:00:08 +0700

    detailsfood

---
+++ CanteenVanLang/Controllers/FoodController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CanteenVanLang.Models;
+
+
+namespace CanteenVanLang.Controllers
+{
+    public class FoodController : Controller
+    {
+        QUANLYCANTEENEntities model = new QUANLYCANTEENEntities();
+        // GET: Food
+        public ActionResult Index()
+        {
+            var listFoods = model.FOODs.OrderByDescending(x => x.ID).ToList();
+            return View(listFoods);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(400, "Bad Request");
+            }
+            FOOD food = model.FOODs.Find(id);
+            if (food == null)
+            {
+                return HttpNotFound("Not Found");
+            }
+            ViewData["list"] = model.FOODs.Where(x => x.ID == id).ToList<FOOD>();
+            return View(food);
+        }
+    }
+}
