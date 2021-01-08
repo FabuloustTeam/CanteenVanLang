cc8982a1fd6c8eb0be669a1b12e49ee5ce036828
187PM13983 - Phan Nguyen Thi Yen Nhi - K24T-PM1 <nhi.187pm13983@vanlanguni.vn>
2021-01-08 20:50:41 +0700

    add validation on model menu

--- CanteenVanLang/Areas/Admin/Controllers/MenuController.cs
+++ CanteenVanLang/Areas/Admin/Controllers/MenuController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 using System.Web;
 using System.Web.Mvc;
+using CanteenVanLang.Areas.Admin.Middleware;
 using CanteenVanLang.Models;
 
 namespace CanteenVanLang.Areas.Admin.Controllers
 {
+    [LoginVertification]
     public class MenuController : Controller
     {
         QUANLYCANTEENEntities model = new QUANLYCANTEENEntities();
@@ -24,6 +27,12 @@ namespace CanteenVanLang.Areas.Admin.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //public JsonResult Create(MENU newMenu)
+        //{
+            
+        //}
+
         [HttpPost]
         public JsonResult getPrice(string foodCode)
         {
@@ -31,18 +40,42 @@ namespace CanteenVanLang.Areas.Admin.Controllers
             return Json(new { success = true, value = price }, JsonRequestBehavior.AllowGet);
         }
 
-        //[HttpPost]
-        //public ActionResult Create(MENU newMenu)
-        //{
-
-        //}
-
         [HttpGet]
         public ActionResult Update(int id)
         {
             var menu = model.MENUs.FirstOrDefault(men => men.ID == id);
             ViewBag.Foods = model.FOODs.OrderBy(food => food.FOOD_NAME).ToList();
             return View(menu);
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            using (var scope = new TransactionScope())
+            {
+                var menu = model.MENUs.FirstOrDefault(f => f.ID == id);
+
+                if (menu.STATUS || checkIfMenuInAnyOrder(id))
+                {
+                    return Json(new { success = false, response = "cannotdelete" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    model.MENUs.Remove(menu);
+                    model.SaveChanges();
+                    
+                    scope.Complete();
+                    return Json(new { success = true, response = "deleted" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        private bool checkIfMenuInAnyOrder(int id)
+        {
+            if (model.ORDER_DETAIL.Where(ordDetail => ordDetail.MENU_ID == id) != null)
+                return true;
+            else
+                return false;
         }
     }
 }
